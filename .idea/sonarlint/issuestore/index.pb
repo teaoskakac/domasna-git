e26a91d2168a482f762ccdd7199c2fc16c0e0dff
Tea <teaoskakac@gmail.com>
2024-02-26 18:10:52 +0100

    Added new class Web

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+I
+src/com/company/Main.java,4\a\4a831b10439099d44774d0ccc387e808deffaeab
+H
+src/com/company/Web.java,f\5\f53fc0d4b7456faa1a61c93191b962945e5e553a
---
+++ src/com/company/Web.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Web {
+    public static void web(String[] args) {
+        System.out.println("Hello domasna!");
+        System.out.println("Test");
+        System.out.println("PlanWeb");
+    }
+}
